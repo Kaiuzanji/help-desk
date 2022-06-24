f003626b302ca8b23c06093bd6b4496bc897dc81
paulo soares <paulo.soares@normatel.com.br>
2022-06-24 00:30:14 -0300

    remove trace css in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.tsx",
     "./src/components/**/*.tsx",
-    "./src/components/*.tsx",
-    "./src/*.tsx"
+    "./src/components/*.tsx"
   ],
   theme: {
     extend: {},
